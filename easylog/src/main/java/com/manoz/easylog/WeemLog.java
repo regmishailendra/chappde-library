b750466bc26d3dd2e170b21f02283019f81cff07
chandra saud <csaud70@gmail.com>
2017-12-18 11:55:30 +0545

    chappde log

---
+++ easylog/src/main/java/com/manoz/easylog/WeemLog.java
@@ -0,0 +1,17 @@
+package com.manoz.easylog;
+
+import android.util.Log;
+
+/**
+ * Created by bidhee on 12/18/17.
+ */
+public class WeemLog {
+
+
+    public static void chhapde(String s){
+Log.v("meroweemkolog",s);
+
+
+    }
+
+}
